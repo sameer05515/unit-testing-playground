c87918e43e80b4eb772ee264c0f1939ef021fe7b
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-09-05 17:36:02 +0530

    commit

--- example-base-05/read-sub-dirs/readSubdirsRecursive.v2.js
+++ example-base-05/read-sub-dirs/readSubdirsRecursive.v2.js
@@ -38,8 +38,51 @@ function readSubdirsRecursive(dirPath, parentId = "") {
 
 // Usage
 // const targetDir = process.argv[2] || '.';
-const folderPath = "D:/Prem";
-const result = readSubdirsRecursive(folderPath)||[];
+const subDirs = [
+  "05-july-2016",
+  "21-dec-2015-home-pc",
+  "21-dec-2015-office",
+  "21-jul-19-oppo",
+  "aerobics-natya",
+  "APPLN_SERVERS-office",
+  "Backup before ubuntu",
+  // "bkp30jun18",
+  "CUSTOM INSTALLATIONS-office",
+  "deskop-bkp-30jun18",
+  "ebooks-home-pc",
+  "home-pc",
+  "java 9",
+  "KT-Documents",
+  "movies",
+  "mum 507",
+  "Narendra",
+  "Negotiations",
+  "new downloads",
+  "Novelvox",
+  "old movies 08-Apr-18",
+  "oppo-06-aug-17",
+  "oppo-10jan19",
+  "Oppo-Backup-15oct2018",
+  "oppo-home-pc",
+  "oppo-mobile-data-05-Jan-18",
+  "readings-office",
+  "rimjhim",
+  "Sandeep Singh",
+  // "segate-files",
+  "SELF",
+  "songs",
+  "Sup",
+  "TUTORIAL",
+  "tutorials",
+  "VANDANA",
+  "you tube videos",
+];
+const folderPath = "E:";
+const result = [];
+subDirs.forEach((sub) => {
+  result.push(...(readSubdirsRecursive(`${folderPath}/${sub}`) || []));
+});
+// const result = readSubdirsRecursive(folderPath) || [];
 // console.log(JSON.stringify(result, null, 1));
-console.log(result.length)
+console.log(result.length);
 FileRelatedOperations.writeFileContentSync(`D:\\v-dir\\content.json`, JSON.stringify(result));
