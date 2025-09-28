557572eee5efb93eef46665a4eae66f21fb63a1a
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-09-28 16:33:34 +0530

    commit

--- example-base-05/express-cgpt-analyse/src/index.v1.js
+++ example-base-05/express-cgpt-analyse/src/index.v1.js
@@ -1,12 +1,6 @@
 const { GIT_REPO, CgptProjectRoot, CGPT_SNAPSHOT_FILE_LOCATION }=require("./constants");
+const {printStepLog}=require("./printStepLog");
 
-const { holiSpecialLog, HoliSpecialColors } = require("./holiSpecialLog");
-
-const printStepLog = (title = "", stepOutput = "") => {
-  console.log("-------------------------------------");
-  console.log(HoliSpecialColors.YELLOW, title);
-  console.log(HoliSpecialColors.GREEN, stepOutput, "\n");
-};
 
 printStepLog("GIT_REPO", GIT_REPO);
 printStepLog("CgptProjectRoot", CgptProjectRoot);
