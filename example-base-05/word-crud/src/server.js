4ceb11ba0b0e169da0f52ef6eee03dbb8bfd32d2
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-10-11 15:06:17 +0530

    commit

--- example-base-05/word-crud/src/server.js
+++ example-base-05/word-crud/src/server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 const PORT = 3030;
 
-app.use(express.json());
+// app.use(express.json());
 app.use(express.static("public"));
+// Increase the limit for JSON and URL-encoded payloads
+app.use(bodyParser.json({ limit: "100mb" }));
+app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
+
 
 // Mount words API routes
 const wordsRouter = require("./routes/words");
